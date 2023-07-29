c030d34da150901313b1d9c06d4214c2f4f2067d
Daniel Mercado <daniel.mercado@puntoscolombia.com>
2023-07-29 18:02:30 -0500

    cors

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const initRoutes = require('./routes');
+const cors = require('cors');
 const { logError, handlerError, boomHandlerError } = require('./middlewares/errorHandler');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+
+//dominios permitidos in whitelist
+const whitelist = ['http://127.0.0.1:5500', 'http://my-app.com'];
+app.use(cors({
+  origin: (origin, cb) => {
+    if (whitelist.includes(origin) || !origin) {
+      cb(null, true);
+    }else{
+      cb(new Error('No permitido'));
+    }
+  }
+}))
 
 app.get('/', (req, res) => {
   res.send('Welcome to my application');
